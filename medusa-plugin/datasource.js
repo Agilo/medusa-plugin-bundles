3431a1dced95b829858bc4012a7e9152a2ca017a
Ivo Limić <ilimic1@gmail.com>
2023-10-13 11:50:47 +0200

    fixing migrations generator.

--- medusa-plugin/datasource.js
+++ medusa-plugin/datasource.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const { DataSource } = require("typeorm");
 
 const AppDataSource = new DataSource({
-  type: process.env.DATABASE_TYPE,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-  database: process.env.DATABASE_NAME,
+  type: process.env.DATABASE_TYPE || "postgres",
+  port: Number(process.env.DATABASE_PORT || 5432),
+  username: process.env.DATABASE_USER || "postgres",
+  password: process.env.DATABASE_PASS || "postgres",
+  database: process.env.DATABASE_NAME || "medusa-docker",
   entities: [
-    "../node_modules/@medusajs/medusa/dist/models/*.js",
+    "node_modules/@medusajs/medusa/dist/models/*.js",
     "dist/models/*.js",
   ],
   migrations: [
-    "../node_modules/@medusajs/medusa/dist/migrations/*.js",
+    "node_modules/@medusajs/medusa/dist/migrations/*.js",
     "dist/migrations/*.js",
   ],
 });

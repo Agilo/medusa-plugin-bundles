0d01354e60327f1dbcf9e408aafda65db752e3ef
Ivo Limić <ilimic1@gmail.com>
2023-10-19 13:59:07 +0200

    adding our MedusaStoreProvider to storefront.

--- dev/medusa-storefront/src/modules/providers/index.tsx
+++ dev/medusa-storefront/src/modules/providers/index.tsx
@@ -5,6 +5,7 @@ import { AccountProvider } from "@lib/context/account-context"
 import { CartDropdownProvider } from "@lib/context/cart-dropdown-context"
 import { MobileMenuProvider } from "@lib/context/mobile-menu-context"
 import { StoreProvider } from "@lib/context/store-context"
+import { MedusaStoreProvider } from "medusa-plugin-bundles/store-client"
 import { MedusaProvider, CartProvider } from "medusa-react"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
@@ -15,15 +16,22 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         client: queryClient,
       }}
     >
-      <CartDropdownProvider>
-        <MobileMenuProvider>
-          <CartProvider>
-            <StoreProvider>
-              <AccountProvider>{children}</AccountProvider>
-            </StoreProvider>
-          </CartProvider>
-        </MobileMenuProvider>
-      </CartDropdownProvider>
+      <MedusaStoreProvider
+        baseUrl={MEDUSA_BACKEND_URL}
+        queryClientProviderProps={{
+          client: queryClient,
+        }}
+      >
+        <CartDropdownProvider>
+          <MobileMenuProvider>
+            <CartProvider>
+              <StoreProvider>
+                <AccountProvider>{children}</AccountProvider>
+              </StoreProvider>
+            </CartProvider>
+          </MobileMenuProvider>
+        </CartDropdownProvider>
+      </MedusaStoreProvider>
     </MedusaProvider>
   )
 }

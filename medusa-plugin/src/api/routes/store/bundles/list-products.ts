f0bd5c35575fb9e40edf59e7c43aaca346039350
Ivo Limić <ilimic1@gmail.com>
2023-11-03 15:29:30 +0100

    removing console.logs.

--- medusa-plugin/src/api/routes/store/bundles/list-products.ts
+++ medusa-plugin/src/api/routes/store/bundles/list-products.ts
@@ -218,11 +218,7 @@ export default async (req, res) => {
   } = req.filterableFields;
   const listConfig = req.listConfig;
 
-  // console.log("listConfig", listConfig);
-
   const bundleProductIds = await bundleService.listProductIds(id);
-  // console.log("bundleProductIds", bundleProductIds);
-  // console.log("filterableFields1", filterableFields);
 
   if (filterableFields.id && typeof filterableFields.id === "string") {
     filterableFields.id = [filterableFields.id];
@@ -236,8 +232,6 @@ export default async (req, res) => {
   } else {
     filterableFields.id = bundleProductIds;
   }
-
-  // console.log("filterableFields2", filterableFields);
 
   if (!filterableFields.id.length) {
     res.json({

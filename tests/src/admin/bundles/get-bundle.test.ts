d24c060d9562bb77d2961cd290cad4f50bb37075
Ivo Limić <ilimic1@gmail.com>
2023-11-07 14:51:56 +0100

    adding admin get-bundle test.

---
+++ tests/src/admin/bundles/get-bundle.test.ts
@@ -0,0 +1,67 @@
+import { isObject } from "lodash";
+import { describe, expect, it } from "vitest";
+import config from "../../config";
+import { recursiveStripProps } from "../../utils";
+
+function testAndSanitizeData(data: any) {
+  expect(isObject(data)).toBe(true);
+  expect(isObject(data.bundle)).toBe(true);
+
+  expect(typeof data.bundle.created_at).toBe("string");
+  expect(typeof data.bundle.updated_at).toBe("string");
+
+  recursiveStripProps(data, [
+    "data.bundle.created_at",
+    "data.bundle.products.created_at",
+    "data.bundle.products.updated_at",
+    "data.bundle.updated_at",
+  ]);
+}
+
+describe("/admin/bundles/get-bundle", () => {
+  it("should return a bundle", async () => {
+    const id = "bundle_coffeemugs01";
+    const response = await fetch(`${config.apiUrl}/admin/bundles/${id}`, {
+      headers: {
+        "x-medusa-access-token": config.apiToken,
+      },
+    });
+    const data = await response.json();
+
+    testAndSanitizeData(data);
+
+    expect({ data, status: response.status }).toMatchFileSnapshot(
+      `../../fixtures/admin/bundles/get-bundle/get-bundle-${id}.json`
+    );
+  });
+
+  it("should return a 404 not found response because bundle does not exist", async () => {
+    const id = "bundle_dummy";
+    const response = await fetch(`${config.apiUrl}/admin/bundles/${id}`, {
+      headers: {
+        "x-medusa-access-token": config.apiToken,
+      },
+    });
+    const data = await response.json();
+
+    expect({ data, status: response.status }).toMatchFileSnapshot(
+      `../../fixtures/admin/bundles/get-bundle/get-bundle-${id}.json`
+    );
+  });
+
+  it("should return a bundle even if it is not published", async () => {
+    const id = "bundle_coffeemugs11";
+    const response = await fetch(`${config.apiUrl}/admin/bundles/${id}`, {
+      headers: {
+        "x-medusa-access-token": config.apiToken,
+      },
+    });
+    const data = await response.json();
+
+    testAndSanitizeData(data);
+
+    expect({ data, status: response.status }).toMatchFileSnapshot(
+      `../../fixtures/admin/bundles/get-bundle/get-bundle-${id}.json`
+    );
+  });
+});

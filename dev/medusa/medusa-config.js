508b4c48f0eeab43dbec55dac8540e1710e0454e
Ivo Limić <ilimic1@gmail.com>
2023-11-08 13:33:00 +0100

    setting up GA.

--- dev/medusa/medusa-config.js
+++ dev/medusa/medusa-config.js
@@ -33,7 +33,11 @@ const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
+console.log("DATABASE_URL", DATABASE_URL);
+
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+console.log("REDIS_URL", REDIS_URL);
 
 const plugins = [
   `medusa-fulfillment-manual`,

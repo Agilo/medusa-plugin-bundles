77e39252346ca5021c0ec78a9c1449a495a76346
Ivo Limić <ilimic1@gmail.com>
2023-11-14 16:39:53 +0100

    trying to import useWidgets(...).

--- medusa-plugin-bundles/src/admin/packages/admin-ui-custom/ui/src/domain/bundles/overview/index.tsx
+++ medusa-plugin-bundles/src/admin/packages/admin-ui-custom/ui/src/domain/bundles/overview/index.tsx
@@ -6,6 +6,86 @@ import PlusIcon from "../../../../../../admin-ui/ui/src/components/fundamentals/
 import BodyCard from "../../../../../../admin-ui/ui/src/components/organisms/body-card";
 import TableViewHeader from "../../../../../../admin-ui/ui/src/components/organisms/custom-table-header";
 import AddBundleModal from "../../../components/templates/bundle-modal";
+import { useWidgets } from "@medusajs/admin-ui/ui/src/providers/widget-provider";
+// import { Bundle } from "../../../../../../admin-client";
+// import WidgetErrorBoundary from "@medusajs/admin-ui/ui/src/components/extensions/widget-container/widget-error-boundary";
+
+// type UseWidgetContainerProps<T extends keyof EntityMap> = {
+//   injectionZone: T
+//   entity?: EntityMap[T]
+// }
+
+// export const useWidgetContainerProps = <T extends keyof EntityMap>({
+//   injectionZone,
+//   entity,
+// }: UseWidgetContainerProps<T>) => {
+//   const baseProps = useExtensionBaseProps() satisfies WidgetProps
+
+//   /**
+//    * Not all InjectionZones have an entity, so we need to check for it first, and then
+//    * add it to the props if it exists.
+//    */
+//   if (entity) {
+//     const propKey = injectionZone as keyof typeof PropKeyMap
+//     const entityKey = PropKeyMap[propKey]
+
+//     return {
+//       ...baseProps,
+//       [entityKey]: entity,
+//     }
+//   }
+
+//   return baseProps
+// }
+
+// export const injectionZones = [
+//   // Bundle injection zones
+//   "bundle.details.before",
+//   "bundle.details.after",
+//   "bundle.list.before",
+//   "bundle.list.after",
+// ] as const;
+
+// type InjectionZone = (typeof injectionZones)[number];
+
+// export type Widget = {
+//   origin: string;
+//   Widget: ComponentType<any>;
+// };
+
+// export type EntityMap = {
+//   // Details
+//   "product.details.after": Bundle;
+//   "product.details.before": Bundle;
+//   // List
+//   "product.list.after"?: never | null | undefined;
+//   "product.list.before"?: never | null | undefined;
+// };
+
+// type WidgetContainerProps<T extends keyof EntityMap> = {
+//   injectionZone: T;
+//   widget: Widget;
+//   entity: EntityMap[T];
+// };
+
+// const WidgetContainer = <T extends InjectionZone>({
+//   injectionZone,
+//   widget,
+//   entity,
+// }: WidgetContainerProps<T>) => {
+//   const { Widget, origin } = widget;
+
+//   const props = useWidgetContainerProps({
+//     injectionZone,
+//     entity,
+//   });
+
+//   return (
+//     <WidgetErrorBoundary origin={origin}>
+//       {React.createElement(Widget, props)}
+//     </WidgetErrorBoundary>
+//   );
+// };
 
 const VIEWS = ["bundles"];
 
@@ -13,9 +93,25 @@ const BundleIndex = () => {
   const { t } = useTranslation();
   const [showNewBundle, setShowNewBundle] = useState(false);
 
+  const { getWidgets } = useWidgets();
+
   return (
     <>
       <div className="gap-y-xsmall flex h-full grow flex-col">
+        {/* {
+          // @ts-ignore
+          getWidgets("bundle.list.before").map((w, i) => {
+            return (
+              <WidgetContainer
+                key={i}
+                // @ts-ignore
+                injectionZone={"bundle.list.before"}
+                widget={w}
+                entity={undefined}
+              />
+            );
+          })
+        } */}
         <div className="flex w-full grow flex-col">
           <BodyCard
             forceDropdown={false}

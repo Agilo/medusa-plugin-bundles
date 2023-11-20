0ffa4538cba8a3e46de1169b1189daa3532d86b9
Ivo Limić <ilimic1@gmail.com>
2023-11-20 14:38:37 +0100

    cleaning up the file + using proper icon.

--- medusa-plugin-bundles/src/admin/routes/bundles/page.tsx
+++ medusa-plugin-bundles/src/admin/routes/bundles/page.tsx
@@ -1,30 +1,15 @@
 import { RouteConfig } from "@medusajs/admin";
 import BundleIndex from "../../packages/admin-ui-custom/ui/src/domain/bundles/overview";
-// import { MedusaAdminProvider } from "../../packages/generated/admin-client";
-// import { useQueryClient } from "react-query";
-// import { MedusaProvider } from 'medusa-react';
+import SquaresPlus from "../../packages/admin-ui/ui/src/components/fundamentals/icons/squares-plus";
 
 const Page = () => {
-  // const client = useQueryClient();
-
-  // // client.getDefaultOptions().mutations.
-  // client.defaultMutationOptions;
-
   return <BundleIndex />;
-  // return (
-  //   <MedusaAdminProvider
-  //     baseUrl="http://localhost:9000"
-  //     apiKey="apitoken"
-  //     queryClientProviderProps={{ client }}
-  //     children={<BundleIndex />}
-  //   />
-  // );
 };
 
 export const config: RouteConfig = {
   link: {
     label: "Bundles",
-    // icon: CustomIcon,
+    icon: SquaresPlus,
   },
 };
 
